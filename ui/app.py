d5f9110e3cabb7977d8de4a99613c5a07d314eea
Diana Lastovetska <diana.lastovetska@studio.unibo.it>
2025-10-26 16:02:17 +0100

    optimize fractal rendering with PhotoImage for better performance

--- ui/app.py
+++ ui/app.py
@@ -5,34 +5,30 @@ class FractalZoomerUI:
         self.root = root
         self.root.title("Fractal Zoomer")
         self.root.geometry("800x600")
-        
-        # Fractal parameters - store as instance variables for zoom feature
         self.width = 800
         self.height = 550
         self.xmin, self.xmax = -2.0, 1.0
         self.ymin, self.ymax = -1.5, 1.5
         self.max_iter = 50
 
-        # Create canvas for drawing fractal
+        # Canvas for drawing
         self.canvas = tk.Canvas(self.root, width=800, height=550, bg="white")
         self.canvas.pack(fill="both", expand=True)
-        
-        # Frame for buttons
+
+        # Button panel
         button_frame = tk.Frame(self.root)
         button_frame.pack(fill="x")
-        
-        # Zoom In button
+
         zoom_in_btn = tk.Button(button_frame, text="Zoom In", command=self.zoom_in)
         zoom_in_btn.pack(side="left", padx=10, pady=10)
         
-        # Zoom Out button
         zoom_out_btn = tk.Button(button_frame, text="Zoom Out", command=self.zoom_out)
         zoom_out_btn.pack(side="left", padx=10, pady=10)
-        
-# Draw Mandelbrot on startup
-self.draw_mandelbrot()
 
-def draw_mandelbrot(self):
+        # Draw Mandelbrot on startup
+        self.draw_mandelbrot()
+
+    def draw_mandelbrot(self):
         img = tk.PhotoImage(width=self.width, height=self.height)
         for x in range(self.width):
             for y in range(self.height):
@@ -47,10 +43,9 @@ def draw_mandelbrot(self):
                 shade = 255 - int(n * 255 / self.max_iter)
                 color = f'#{shade:02x}{shade:02x}{shade:02x}'
                 img.put(color, (x, y))
-    self.canvas.create_image((0, 0), anchor="nw", image=img)
-    self.canvas.image = img
+        self.canvas.create_image((0, 0), anchor="nw", image=img)
+        self.canvas.image = img
 
-                
     def zoom_in(self):
         x_range = (self.xmax - self.xmin) * 0.5
         y_range = (self.ymax - self.ymin) * 0.5
@@ -61,7 +56,7 @@ def draw_mandelbrot(self):
         self.ymin = y_center - y_range / 2
         self.ymax = y_center + y_range / 2
         self.draw_mandelbrot()
-        
+
     def zoom_out(self):
         x_range = (self.xmax - self.xmin) * 2
         y_range = (self.ymax - self.ymin) * 2
